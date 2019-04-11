b2e687e670255fe23a7c0a2cd3324d1304fd70ce
Andrew Patterson <apat24@mail.sfsu.edu>
2019-04-11 14:31:54 -0700

    adds boilerplate

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Components
+// Styles
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
+
+ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));
